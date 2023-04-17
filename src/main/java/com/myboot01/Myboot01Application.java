7e9de2990995b0430d8f191d2cd32c65ae9f5924
StandOut <StandOut.first@gmail.com>
2023-04-17 11:40:53 +0900

    첫 화면출력해보기

--- src/main/java/com/myboot01/Myboot01Application.java
+++ src/main/java/com/myboot01/Myboot01Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Myboot01Application {
 
+	//프로젝트이름 + Application 형태로 메인클래스가 자동작성됨.
+	//일반자바 어플리케이션, 웹애플리케이션으로 실행가능.
 	public static void main(String[] args) {
 		SpringApplication.run(Myboot01Application.class, args);
 	}
